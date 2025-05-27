aff29151a92b1da1d52569dc82eaf64c8ad453f2
weir1 <74604555+weir1@users.noreply.github.com>
2025-05-27 15:06:16 +0530

    Update pornolab.py

--- pornolab.py
+++ pornolab.py
@@ -6,8 +6,8 @@
 
 # Replace YOUR_USERNAME_HERE and YOUR_PASSWORD_HERE with your Pornolab username and password
 credentials = {
-    'login_username': '',
-    'login_password': '',
+    'login_username': 'imbest',
+    'login_password': 'maushi1230',
 }
 
 # Logging
